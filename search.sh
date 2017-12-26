dab6af69f320e71e08e6c4d9c4d16fe42a8bb01f
Kenneth Berland <ken@hero.com>
2017-12-25 23:29:25 -0800

    foo

--- search.sh
+++ search.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+set -x
 TARGET=$HOME/mail/mairix
 rm -rf $TARGET
 rm -rf $HOME/dcm/.mairix
-mairix $1
+
+THIS_YEAR=$(date +%Y)
+LAST_YEAR=$(($THIS_YEAR - 1))
+TODAY=$(date +%m%d)
+
+mairix d:${LAST_YEAR}${TODAY}-${THIS_YEAR}${TODAY} $@
 mv $TARGET $HOME/dcm/.mairix
 alpine -f mairix
 
--- secure.sh
+++ secure.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-# pgpe -r"ken@hero.com" plain.txt
-# pgpv plain.txt.pgp
-	     
 if (( $# == 1 )); then
     echo "read only"
     echo "decrypting file cryptext with pgp"
     gpg -d --output plain.txt plain.txt.gpg
-    # pgpv plain.txt.pgp
     emacs -nw ./plain.txt
     rm ./plain.txt ./plain.txt~
 else
     echo "read/write"
     cp ./plain.txt.gpg ./cryptext.$$
-    # pgpv plain.txt.pgp
     gpg -d --output plain.txt plain.txt.gpg
     emacs -nw ./plain.txt
     gpg -e -r CCFDABBD plain.txt
-    # pgpe -r"ken@hero.com" plain.txt
     rm ./plain.txt ./plain.txt~
 fi
 
