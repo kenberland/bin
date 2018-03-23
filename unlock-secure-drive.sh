a376f0746f4e9d7a9efd84ebdd1e7f9f0d2c64ac
Kenneth Berland <ken@hero.com>
2018-03-22 21:00:00 -0700

    add autocomplete to scan

--- unlock-secure-drive.sh
+++ unlock-secure-drive.sh
@@ -9,13 +9,12 @@ echo
 
 HD_PASS=$(echo $PASS | gpg --passphrase-fd 0 -d $HOME/ST1000NM0033-9ZM173-Z1W5ZKFL-4b074c67-3f31-4a39-a724-467b9f7e914a.password.txt.gpg)
 
-echo $PASS | sudo -S -i <<EOF
-echo "- - -" | tee /sys/class/scsi_host/host6/scan
-hdparm --security-unlock $HD_PASS /dev/sdc >& /dev/null
-partprobe
-umount /mnt/sdc1
-mount /mnt/sdc1
-EOF
+echo $PASS | sudo -S -i whoami
+echo "- - -" | sudo tee /sys/class/scsi_host/host6/scan
+sudo hdparm --security-unlock $HD_PASS /dev/sdb >& /dev/null
+sudo partprobe
+sudo umount /mnt/sdc1
+sudo mount /mnt/sdc1
 
 #cat > /tmp/${0##*/}.$$.tmp <<EOF
 #EOF
