203aa458ecca92e165186d44fa4a20223282a4d2
Kenneth Berland <ken@hero.com>
2018-07-13 10:50:35 -0700

    mod search

--- search.sh
+++ search.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
+
 TARGET=$HOME/mail/mairix
 rm -rf $TARGET
 rm -rf $HOME/dcm/.mairix
@@ -7,8 +8,15 @@ rm -rf $HOME/dcm/.mairix
 THIS_YEAR=$(date +%Y)
 LAST_YEAR=$(($THIS_YEAR - 1))
 TODAY=$(date +%m%d)
+DATE_RANGE=d:${LAST_YEAR}${TODAY}-${THIS_YEAR}${TODAY}
 
-mairix d:${LAST_YEAR}${TODAY}-${THIS_YEAR}${TODAY} $@
+if [ "$#" != "1" ]; then
+    shift
+    mairix $@
+else
+    mairix $DATE_RANGE $@
+fi
+
 mv $TARGET $HOME/dcm/.mairix
 alpine -f mairix
 
