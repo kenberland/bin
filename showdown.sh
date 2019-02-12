25936ecc9942214f0afd74f2b83e20bfb4cb7073
Kenneth Berland <berlandk@amazon.com>
2019-02-12 11:01:33 -0800

    add tables

--- showdown.sh
+++ showdown.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+env >> /tmp/foo.txt
 TEMPFILE="/tmp/foo.$$.md"
 while read line
 do
   echo "$line" >> "$TEMPFILE"
 done < "${1:-/dev/stdin}"
-showdown makehtml -i "$TEMPFILE" 2>/dev/null
+showdown makehtml -i "$TEMPFILE" --tables 2>/dev/null
 
