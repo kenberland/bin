3598b56750b21249c5058a1b6c746e29901d0920
Kenneth Berland <ken@hero.net>
2026-01-21 08:28:58 -0800

    fix buses

--- izzy-buses/buses.sh
+++ izzy-buses/buses.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-cd $HOME/izzy-buses
-export LD_LIBRARY_PATH=/usr/local/lib:/home/ken/hero_backup_exclude/ssl-1.1.1k/lib
+cd $HOME/bin/izzy-buses
 export TZ=America/Los_Angeles
-source .venv/bin/activate
 source ./secrets.txt
-./buses.py
+uv run python ./buses.py
 
